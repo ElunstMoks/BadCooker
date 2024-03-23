e74386643906ff1f52984ca9b68fd3c5d3864b7d
Andrew Rudenko <andrewrudenko@MacBook-Air-Andrew.local>
2024-03-23 18:42:22 +0200

    'game time' and 'cash' unit counters finished

--- unit_resources/cash.py
+++ unit_resources/cash.py
@@ -1,2 +1,115 @@
+from typing import Self, Optional
+import constraints
+
 class Cash(int):
-    pass
+    def __new__(cls, value: int):
+        """
+        >>> Cash(40)
+        Cash(40)
+        """
+        return super().__new__(cls, value)
+
+    def __sub__(self, other) -> Self:
+        """
+        >>> cash = Cash(40)
+        >>> cash
+        Cash(40)
+
+        >>> cash -= 20
+        >>> cash
+        Cash(20)
+
+        >>> cash -= 30 # doctest: +ELLIPSIS
+        Traceback (most recent call last):
+        constraints.CashDefecit: ...
+
+        >>> cash -= -10
+        >>> cash
+        Cash(30)
+        """
+        return self.spend(other, allow_debts=False)
+
+    def spend(self, /, value: int, *, allow_debts: bool = False) -> Self:
+        """
+        reduces a player's cash
+        allow_debts - if True disables the cash defecit constraint - player's cash now can be negative
+        >>> cash = Cash(40)
+        >>> cash
+        Cash(40)
+
+        >>> cash = cash.spend(20)
+        >>> cash
+        Cash(20)
+
+        >>> cash.spend(30) # doctest: +ELLIPSIS
+        Traceback (most recent call last):
+        constraints.CashDefecit: ...
+
+        >>> cash = cash.spend(30, allow_debts=True)
+        >>> cash
+        Cash(-10)
+
+        >>> cash = cash.spend(-10)
+        >>> cash
+        Cash(0)
+        """
+        if value < 0:
+            return self + -value
+        if not allow_debts and super().__sub__(value) < 0:
+            raise constraints.CashDefecit()
+        return Cash(super().__sub__(value))
+
+    def __add__(self, other: int) -> Self:
+        """
+        >>> cash = Cash(40)
+        >>> cash
+        Cash(40)
+
+        >>> cash += 10
+        >>> cash
+        Cash(50)
+
+        >>> cash += -20
+        >>> cash
+        Cash(30)
+        """
+        if other < 0:
+            return self - -other
+        return Cash(super().__add__(other))
+
+    def __str__(self) -> str:
+        """
+        a string of player's cash for the game interface
+        >>> print(Cash(40))
+        [bold yellow]40[/] :coin:
+
+        >>> print(Cash(-10))
+        [bold magenta]-10[/] :coin:
+        """
+        color = "magenta" if self.debts else "yellow"
+        return f"[bold {color}]{int(self)}[/] :coin:"
+
+    def __repr__(self) -> str:
+        """
+        >>> Cash(40)
+        Cash(40)
+        """
+        return f"{self.__class__.__name__}({super().__repr__()})"
+
+    @property
+    def debts(self) -> Optional[int]:
+        if self >= 0:
+            return None
+        return -int(super().__repr__())
+
+    def __int__(self) -> int:
+        """
+        integer representation of the cash
+        >>> int(Cash(40))
+        40
+        """
+        return int(super().__repr__())
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True, optionflags=doctest.ELLIPSIS | doctest.REPORT_ONLY_FIRST_FAILURE)
--- unit_resources/game_time.py
+++ unit_resources/game_time.py
@@ -1,12 +1,12 @@
-from typing import Self
+from typing import Self, Optional
 import constraints
 
 class GameTime(int):
     DAILY_TIME = 1000
 
-    def __new__(cls, *args, **kwargs):
+    def __new__(cls, value: int):
         """
-        >>> repr(GameTime(120))
+        >>> GameTime(120)
         GameTime(120)
 
         >>> GameTime(2000)
@@ -17,50 +17,51 @@ class GameTime(int):
         Traceback (most recent call last):
         TypeError: GameTime(-800) not in valid range
         """
-        new = cls(*args, **kwargs)
-        if new not in range(cls.DAILY_TIME // -2, cls.DAILY_TIME):
-            raise TypeError(f"{new!r} not in valid range")
-        return new
+        if value not in range(cls.DAILY_TIME // -2, cls.DAILY_TIME + 1):
+            raise TypeError(f"{cls.__name__}({value}) not in valid range")
+        return super().__new__(cls, value)
 
     def __add__(self, other: int) -> Self:
         """
         adds extra time to a player's current game day
-
         >>> game_time = GameTime(120)
         >>> game_time += 170
-        >>> repr(game_time)
+        >>> game_time
         GameTime(290)
+
         >>> game_time += 800
-        >>> repr(game_time)
+        >>> game_time
         GameTime(1000)
+
         >>> game_time += -150
-        >>> repr(game_time)
+        >>> game_time
         GameTime(850)
         """
         if other < 0:
             return self - -other
-        return min(super().__add__(other), GameTime.DAILY_TIME)
+        return GameTime(min(super().__add__(other), GameTime.DAILY_TIME))
 
     def __sub__(self, other: int) -> Self:
         """
         reduces time of a player's game day
-
         >>> game_time = GameTime(100)
         >>> game_time -= 120
-        >>> repr(game_time)
+        >>> game_time
         GameTime(-20)
-        >>> game_time -= 600
+
+        >>> game_time -= 600 # doctest: +ELLIPSIS
         Traceback (most recent call last):
-        constraints.TimeDefecit: it's brings you to unallowed tiedness level
+        constraints.TimeDefecit: ...
+
         >>> game_time -= -60
-        >>> repr(game_time)
+        >>> game_time
         GameTime(40)
         """
         if other < 0:
-            return self + other
+            return self + -other
         if self + GameTime.DAILY_TIME // 2 < other:
             raise constraints.TimeDefecit()
-        return super().__sub__(other)
+        return GameTime(super().__sub__(other))
 
     def __mul__(self, other) -> Self:
         return NotImplemented
@@ -71,30 +72,63 @@ class GameTime(int):
     def __str__(self) -> str:
         """
         a string displaying a current game time for the game interface
+        >>> print(GameTime(120))
+        [bold black]120[/] :seven o'clock:
 
-        >>> GameTime(120)
-        120 🕗
+        >>> print(GameTime(-10))
+        [bold magenta]10[/]
         """
-        color = "magenta" if self.with_tiedness else "black"
-        return f"[bold {color}]{super().__str__()}[/] :clock:"
+        indicator = self.tiredness or int(self)
+        color = "magenta" if self.tiredness else "black"
+        emoji = "seven o'clock" if not self.tiredness else None
+        return f"[bold {color}]{indicator}[/]" + (f" :{emoji}:" if emoji else "")
 
     def __repr__(self) -> str:
         """
-        >>> repr(GameTime(120))
+        >>> GameTime(120)
         GameTime(120)
         """
-        return f"{self.__class__.__name__}({super().__str__()})"
+        return f"{self.__class__.__name__}({super().__repr__()})"
 
     @property
-    def with_tiedness(self) -> bool:
+    def tiredness(self) -> Optional[int]:
         """
-        checks where's a player is 'tied', tiedness is represented as a negative game time
+        a factor of game time's way below zero
+        >>> GameTime(120).tiredness
 
-        >>> GameTime(120).with_tiedness
-        False
-        >>> GameTime(0).with_tiedness
-        False
-        >>> GameTime(-80).with_tiedness
-        True
+        >>> GameTime(-60).tiredness
+        60
         """
-        return self <= 0
+        if self >= 0:
+            return None
+        return int(self.negate())
+
+    def negate(self) -> Self:
+        """
+        returns an exact tiredness, if player's time is positive or an exact positive time if this game time
+        is represented in tiredness
+        >>> GameTime(120).negate()
+        GameTime(-120)
+
+        >>> GameTime(-40).negate()
+        GameTime(40)
+
+        >>> GameTime(600).negate() # doctest: +ELLIPSIS
+        Traceback (most recent call last):
+        constraints.TimeDefecit: ...
+        """
+        if self > GameTime.DAILY_TIME // 2:
+            raise constraints.TimeDefecit()
+        return GameTime(super().__mul__(-1))
+
+    def __int__(self) -> int:
+        """
+        integer representation of this game time
+        >>> int(GameTime(120))
+        120
+        """
+        return int(super().__repr__())
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=False, optionflags=doctest.ELLIPSIS | doctest.REPORT_ONLY_FIRST_FAILURE)
